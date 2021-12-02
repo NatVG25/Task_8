24a0efc3a5ef1e6d845b5abdc758899af4818927
Goliash <Goliash@KONSTRUKTOR4>
2021-12-02 09:05:46 +0500

    Добавьте файлы проекта.

---
+++ Task_8.2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Task_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\ИТМО\С#\TASK\Task_8\NumberSum.txt"; //создаем текстовый файл по указанному пути
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+                
+            int[] array = new int[10]; //создаем массив рандомных чисел
+            Random random = new Random();
+            
+            using (StreamWriter sw = new StreamWriter(path)) //записываем массив рандомных чивел в текстовый файл
+            { 
+               for (int i = 0; i < 10; i++)
+               {
+                    array[i] = random.Next(0, 10);
+                    sw.WriteLine("{0} ", array[i]);
+               }
+             }
+            
+            string[] lines = File.ReadAllLines(path); //считываем все строки текстового файла в массив и выводим массив на консоль
+            
+            foreach (string s in lines)
+            {
+                Console.WriteLine(s);
+            }
+
+
+            int Sum = 0;
+            for (int i = 0; i <= 10; i++)
+            {
+                
+                Sum += Convert.ToInt32(i); //находим сумму чисел, конвертируя строковые значения в числовые
+            }
+
+            Console.WriteLine("Сумма 10 произвольных чисел равна {0}", Sum);//выводим результат сложения
+            Console.ReadKey();
+         }
+    }
+}
---
+++ Task_8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\ИТМО\С#"; //указываем абсолютный путь к нужной папке
+            string s = "*.*"; 
+            DirectoryInfo directory = new DirectoryInfo(path); //создаем экземпляр класса конструктором - аргумент путь к каталогу
+            DirectoryInfo[] directories = directory.GetDirectories(s, SearchOption.AllDirectories); //создаем список папок в главном каталоге (в аргументах указываем путь и что ищем в текущей папке и во вложенных)
+            Console.WriteLine(@"{0}\", path); //выводим путь основной папки
+            FileInfo[] files = directory.GetFiles(); //создаем список файлов в папках первого уровня
+            foreach (FileInfo file in files) //перебираем файлы первого уровня
+            {
+                Console.WriteLine("-{0}", file.Name); //выводим названия файлов первого уровня
+            }
+            Console.WriteLine();
+
+            foreach (DirectoryInfo direct in directories) //перебираем вложенные каталоги 
+            {
+                Console.WriteLine(@"{0}\", direct.FullName); //выводим пути вложенных каталогов
+                string name = direct.FullName;
+                DirectoryInfo nameSub = new DirectoryInfo(name);
+
+                foreach (FileInfo file in nameSub.GetFiles()) //перебираем файлы во вложенных каталогах
+                {
+                    Console.WriteLine("-{0}", file.Name); //выводим названия файлов
+                }
+                Console.WriteLine();
+            }
+                Console.ReadKey();
+            
+        } 
+    }
+}
